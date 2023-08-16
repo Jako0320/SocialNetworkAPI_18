2962ac548c5178c427b385046129568722b00c16
Jacques <jacques.scoffoni@gmail.com>
2023-08-16 00:09:31 -0400

    File and folder directory and Models created

---
+++ models/Thought.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toISOString(),
+  },
+});
+
+const thoughtSchema = new Schema(
+  {
+    _id: {
+      type: Schema.Types.ObjectId,
+      auto: true,
+    },
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    userName: {
+      type: String,
+      default: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toISOString(),
+    },
+    reactions: [reactionSchema],   
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
